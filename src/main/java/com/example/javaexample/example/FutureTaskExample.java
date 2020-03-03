57e76dfaf4d9834af987f0398d1919b9df1ef695
meng_wei <meng_wei@suixingpay.com>
2020-03-03 16:49:55 +0800

    add FutureTaskExample.java NoThreadSafeExample.java

---
+++ src/main/java/com/example/javaexample/example/FutureTaskExample.java
@@ -0,0 +1,28 @@
+package com.example.javaexample.example;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author: meng_wei[meng_wei@suixingpay.com]
+ * @date: 2020/3/2 18:40
+ * @version: V1.0
+ * @desc
+ */
+public class FutureTaskExample implements Callable<Integer> {
+
+  @Override
+  public Integer call() throws Exception {
+    return new Random().nextInt(100);
+  }
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    FutureTask<Integer> futureTask = new FutureTask<>(new FutureTaskExample());
+    Thread thread = new Thread(futureTask);
+    thread.start();
+    Integer res = futureTask.get();
+    System.out.println("res:"+res);
+  }
+}
---
+++ src/main/java/com/example/javaexample/example/NoThreadSafeExample.java
@@ -0,0 +1,51 @@
+package com.example.javaexample.example;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author: meng_wei[meng_wei@suixingpay.com]
+ * @date: 2020/3/2 18:56
+ * @version: V1.0
+ * @desc no-thread-safe 可以加锁 或者 AtomicInteger
+ */
+public class NoThreadSafeExample {
+
+  private int num;
+  private Object lock = new Object();
+
+  public void add(){
+    //thread-safe
+//    synchronized (lock){
+//      num++;
+//    }
+    //no-thread-safe
+    num++;
+  }
+
+  public int get(){
+    return num;
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    final int threadNum = 1000;
+    NoThreadSafeExample noThreadSafeExample = new NoThreadSafeExample();
+    CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    for (int i = 0; i < threadNum; i++) {
+      executorService.execute(new Runnable() {
+        @Override
+        public void run() {
+          noThreadSafeExample.add();
+          countDownLatch.countDown();
+        }
+      });
+      System.out.println("execing...");
+    }
+    countDownLatch.await();
+    System.out.println("res:"+noThreadSafeExample.get());
+    executorService.shutdown();
+  }
+
+}
