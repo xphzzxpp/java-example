66271ef1adb40b20a73913cdc9e1b9e39363b4f5
meng_wei <meng_wei@suixingpay.com>
2020-03-08 18:10:01 +0800

    add LinkedListExample.java

---
+++ src/main/java/com/example/javaexample/example/LinkedListExample.java
@@ -0,0 +1,22 @@
+package com.example.javaexample.example;
+
+import java.util.LinkedList;
+
+/**
+ * @author: meng_wei[meng_wei@suixingpay.com]
+ * @date: 2020/3/4 16:35
+ * @version: V1.0
+ * @desc
+ */
+public class LinkedListExample {
+
+  public static void main(String[] args) {
+    LinkedList<String> linkedList = new LinkedList<>();
+    linkedList.add("1");
+    linkedList.add("2");
+    linkedList.add("3");
+    linkedList.add("4");
+
+    linkedList.forEach(o -> System.out.println(o));
+  }
+}
